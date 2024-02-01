9dd0e0f2d7dc7edf7e89a1a1a05cfab8315b14dd
ucctn <chetan.singhal@ucertify.com>
2024-01-31 18:10:57 -0800

    Calculate Tax

--- orderEntryPhase2/src/orderEntryPhase2/OrderEntryPhase2.java
+++ orderEntryPhase2/src/orderEntryPhase2/OrderEntryPhase2.java
@@ -9,11 +9,13 @@ import java.util.Scanner;
 public class OrderEntryPhase2 {
 
 	//vars
+	private double discount;
 	private String itemDescription;
 	private String itemNumber;
 	private double itemPrice;
 	private int itemQtyOrdered;
 	private double netPrice;
+	private double taxPercent;
 	private double totalDiscount;
 	private double totalTax;
 	
@@ -24,6 +26,7 @@ public class OrderEntryPhase2 {
 		getItemNumber();
 		getItemPrice();
 		getOrderedQuantity();
+		getTax();
 	}
 	
 	//Item Description
@@ -62,20 +65,35 @@ public class OrderEntryPhase2 {
 		itemQtyOrdered = oqOBJ.nextInt();
 	}
 	
+	//Tax Percent
+	private void getTax()
+	{
+		Scanner txOBJ = new Scanner(System.in);
+		System.out.println("Enter Tax Percent: ");
+		
+		taxPercent = txOBJ.nextDouble();
+	}
+	
 	
 	//Print Invoice
 	public void printInvoice()
 	{
+		Double grossPrice = itemPrice*itemQtyOrdered;  // calc cost of item
+		totalTax = grossPrice * taxPercent;  // Calc Total Tax
+		
 		System.out.println("\n Order Entry\n");
-		System.out.println("Item Number  \tItem Description  \tItem Price \tQuantity");
-		System.out.println("  "+itemNumber+"\t   "+itemDescription+"\t\t   "+itemPrice+"\t\t    "+itemQtyOrdered);
+		System.out.println("Item Number  \tItem Description  \tItem Price \tQuantity \t Tax % \t Tax");
+		System.out.println("  "+itemNumber+"\t   "+itemDescription+"\t\t   "+itemPrice+"\t\t    "+itemQtyOrdered+"\t\t"+taxPercent+"\t"+totalTax);
 	}
 	
 	//-----------------------------------------------------------------------
 	public static void main(String[] args) {
 		OrderEntryPhase2 oep = new OrderEntryPhase2();
 		
+		// gather input data
 		oep.gatherInputs();
+		
+		// print invoice
 		oep.printInvoice();
 		
 		System.out.println("\nBye!!!");
