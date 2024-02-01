89107f9596adb98ba5dcdeef46012e8b27b7d54a
ucctn <chetan.singhal@ucertify.com>
2024-01-31 17:06:03 -0800

    Start Project

---
+++ orderEntryPhase2/src/orderEntryPhase2/OrderEntryPhase2.java
@@ -0,0 +1,15 @@
+//Thora Cummins
+//CIS406
+//Order Entry Phase 2
+//
+
+package orderEntryPhase2;
+
+public class OrderEntryPhase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
